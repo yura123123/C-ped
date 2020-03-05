ddc17e8fb1366490d11c6ae1ee205b3aa7798440
LudwigVonChesterfield <andreydell@yahoo.com>
2020-03-06 00:55:40 +0200

    Basic Flask setup. (#2)
    * Flask server with WebSocket layer.
    
    - Very basic "drop" prevention.
    - Very basic server setup.
    
    * Making the code a bit more readable.
    
    * IP var, *correct start up message*, import management for errors
    
    * Changes to appease linter.
    
    * Even more linter changes.
    
    * not even comments are safe from linting
    
    * ... commenting out a placeholder smh.

---
+++ code/__run.py
@@ -0,0 +1,36 @@
+from sys import exc_info
+from traceback import print_exception
+
+from server import main as run_server
+
+
+def run():
+    # see server.py for more detail, all server-related stuff is handled there.
+    run_server()
+
+
+if __name__ == "__main__":
+    # Semi-automatic backend restarting in case it dropped for any reason.
+    while True:
+        try:
+            run()
+
+        # Anything in run() called for a forced.
+        except SystemExit:
+            break
+
+        # Exceptions displaying.
+        except Exception as err:
+            try:
+                exc_info = exc_info()
+            finally:
+                # Display the *original* exception
+                print(err)
+                print_exception(*exc_info)
+                del exc_info
+
+        stopper = input().lower()
+        if stopper == "q" or stopper == "quit":
+            break
+
+    print("Stopping server...")
---
+++ code/server.py
@@ -0,0 +1,53 @@
+from os import path
+
+from flask import Flask, render_template, send_from_directory
+from flask_socketio import SocketIO
+
+
+def main():
+    # The path to directory this code is ran in.
+    CUR_PATH = path.dirname(path.abspath(__file__))
+
+    # The IP to run server on.
+    IP = "0.0.0.0"
+    # The port on which server is hosted.
+    PORT = 8080
+    # !!! FUN !!! DEBUG MODE. Gives access to most stuff on server,
+    # very dangerous to be put on production.
+    DEBUG = False
+    # Secret key used to encrypt requests and stuff.
+    SECRET_KEY = "SECRET KEY TO PUT IN CONFIG AND STUFF"
+
+    # The directory with all web-related templates.
+    template_dir = path.join(CUR_PATH, "../templates")
+    # The directory for static files, such as .css, client-side JavaScript.
+    static_dir = path.join(CUR_PATH, "../static")
+
+    app = Flask(__name__, template_folder=template_dir, static_url_path="")
+    app.config["SECRET_KEY"] = SECRET_KEY
+
+    # app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+    socketio = SocketIO(app)
+
+    @app.after_request
+    def after_request(response):
+        response.headers[
+            "Strict-Transport-Security"
+        ] = "max-age=31536000; includeSubDomains"
+        # response.headers['Content-Security-Policy'] = "default-src 'self'"
+        response.headers["X-Content-Type-Options"] = "nosniff"
+        # response.headers['X-Frame-Options'] = 'SAMEORIGIN'
+        response.headers["X-XSS-Protection"] = "1; mode=block"
+        return response
+
+    @app.route("/")
+    def index():
+        return render_template("index.html")
+
+    @app.route("/static/<path:path>")
+    def send_static(path):
+        return send_from_directory(static_dir, path)
+
+    print("=====\n" + "Server starting on " + str(IP) + ":" + str(PORT))
+
+    socketio.run(app, host=IP, port=PORT, debug=DEBUG)
